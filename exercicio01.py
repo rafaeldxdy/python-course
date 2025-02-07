e75f9c3e75724d528eff45509bceea8098c7e9ae
Rafael Ribeiro <107411248+rafaeldxdy@users.noreply.github.com>
2025-02-07 10:29:40 +0000

    exercicio 01

---
+++ exercicio01.py
@@ -0,0 +1,15 @@
+## Exercício com variáveis e tipos de dados
+
+nome = 'Rafael'
+sobrenome = 'Ribeiro'
+idade = 33
+ano_nascimento = 1991
+maior_idade = 'Sim'
+altura_metros = 1.68
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_idade)
+print('Altura em metros:', altura_metros)
