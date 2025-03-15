0428b86791c2c7b17efeb1c108a4ed2cbc2de3c2
Rafael Ribeiro <107411248+rafaeldxdy@users.noreply.github.com>
2025-03-15 23:46:57 +0000

    operadores aritmeticos

---
+++ operadores_artimeticos.py
@@ -0,0 +1,20 @@
+adicao = 1 + 1
+print('Adição:', adicao)
+
+subtracao = 1-1
+print('Subtração:', subtracao)
+
+multiplicacao = 1 * 1
+print('multiplicação:', multiplicacao)
+
+divisao = 1 / 1
+print('Divisão:', divisao)
+
+divisao_inteira = 1 // 1
+print('Divisão inteira:', divisao_inteira)
+
+modulo = 1 % 1
+print('Módulo:', modulo)
+
+exponenciacao = 1 ** 1
+print('Exponenciação:', exponenciacao)
