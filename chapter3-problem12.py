8b69de38efbb6212ea759f5af6b274d4a20473b5
Rafael Ribeiro <rmeribeiro@gmail.com>
2025-06-04 22:58:54 -0300

    ABSWP: Chapter 2 - Problem 12

---
+++ chapter3-problem12.py
@@ -0,0 +1,15 @@
+# Chapter 2, Problem 12 of Automate Boring Stuff with Python
+
+print('range(10):')
+for i in range(10):
+    print(i)
+
+print('range(0, 10):')
+for i in range(0, 10):
+    print(i)
+
+print('range(0, 10, 1):')
+for i in range(0, 10, 1):
+    print(i)
+
+print('Bye, bye!')
