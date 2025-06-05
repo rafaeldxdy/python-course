14412694d4a0a1686e37f0cbbd7a671d42f89943
Rafael Ribeiro <rmeribeiro@gmail.com>
2025-06-04 23:02:25 -0300

    ABSWP: Chapter 2 - Problem 13

---
+++ chapter2-problem13.py
@@ -0,0 +1,12 @@
+# Chapter 2, problem 13 of Automate Boring Stuff with Python
+
+n = 1
+
+print('Using For Statement')
+for i in range(1, 11):
+    print(i)
+
+print('Using While Statement')
+while n <= 10:
+    print(n)
+    n += 1
