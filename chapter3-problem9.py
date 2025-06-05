0c0e205a23ba2bc5a6a384078212c56a7ca3e2cd
Rafael Ribeiro <rmeribeiro@gmail.com>
2025-06-04 22:53:59 -0300

    ABSWP: Chapter 3 - Problem 9

---
+++ chapter3-problem9.py
@@ -0,0 +1,10 @@
+# Problem 9 of chapter 3 of Automate Boring Stuff with Python
+
+spam = int(input('Type a number for spam: '))
+
+if spam == 1:
+    print('Hello')
+elif spam == 2:
+    print('Howdy')
+else:
+    print('Greetings!')
